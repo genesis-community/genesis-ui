d0d63729743e3df8c77a488cd98fe86cf73e5940
Tanvie Kirane <67563504+tanviekirane@users.noreply.github.com>
2021-10-18 20:21:03 -0400

    testing git

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+// Importing fmt
+import (
+	"fmt"
+)
+
+// Calling main
+func main() {
+
+	// Declaring some const variables
+	const name, dept = "GeeksforGeeks", "CS"
+
+	// Calling print() function
+	fmt.Print(name, " is a ", dept, " portal.\n")
+
+}
