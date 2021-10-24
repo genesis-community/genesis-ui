315a3b725f12bec1b5c78ac13aa13e6d4ca3c5e7
Michelle Wong <michellewong@Michelles-MacBook-Pro.local>
2021-10-23 21:35:32 -0400

    Starting login page

--- main.go
+++ main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+	"os"
 )
 
+var tpl = template.Must(template.ParseFiles("login.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
 func main() {
-	var mystring = "Hello world"
-	fmt.Printf("The string is %s", mystring)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
