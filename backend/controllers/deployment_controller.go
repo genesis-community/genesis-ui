81dbe0318eb7291ec8dd9e299ad3e9856b67e3f6
MrForBear <forcodingbear@gmail.com>
2022-03-23 14:32:53 -0400

    fix logic and error message, ready to merge

--- backend/controllers/deployment_controller.go
+++ backend/controllers/deployment_controller.go
@@ -16,20 +16,22 @@ import (
 	"os"
 )
 
-func LoadDeployments(director string) map[string]interface{} {
+func LoadDeployments(path string, name string) map[string]interface{} {
 	client := CreatClient()
+	director := path + name
 	data_path := os.Getenv("VAULT_DATA_PREFIX")
 	result := make(map[string]interface{}) // {"code": http status code, "meg": error message or kit details}
 	secret, err := client.Logical().Read(data_path + director)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "client.Logical().Read(%s): %+v\n", data_path+director, err)
-		result = map[string]interface{}{"code": http.StatusUnauthorized, "meg": "Access error."}
+		result = map[string]interface{}{"code": http.StatusUnauthorized, "meg": "Check your permission."}
 	} else if secret == nil {
 		fmt.Fprintf(os.Stderr, "client.Logical().Read(%s): Secret not found!\n", data_path+director)
-		result = map[string]interface{}{"code": http.StatusNotFound, "meg": "Marco: Give me my file! \n Polo: What file?"}
+		result = map[string]interface{}{"code": http.StatusNotFound, "meg": "Marco: Give me " + name + "! Polo: " + name + "?"}
 	} else {
 		data := secret.Data["data"].(map[string]interface{})
-		keys := []string{"kit_name", "kit_is_dev", "dated", "deployer", "kit_version", "features"}
+		result["kit_name"] = name
+		keys := []string{"kit_is_dev", "dated", "deployer", "kit_version", "features"}
 		for _, key := range keys {
 			if val, ok := data[key]; ok {
 				result[key] = val
@@ -53,8 +55,9 @@ func ListDeployments() gin.HandlerFunc { // for listing deployments and kit deta
 		sub_path := strings.Split(context.Params.ByName("any"), "/")[1:]
 		if len(sub_path) == 2 { // snw-name-lab/kit-name to get kit details
 			if sub_path[len(sub_path)-1] != "" {
-				path := sub_path[0] + "/" + sub_path[1]
-				kit_detail := LoadDeployments(path)
+				path := sub_path[0] + "/"
+				name := sub_path[1]
+				kit_detail := LoadDeployments(path, name)
 				context.JSON(kit_detail["code"].(int), kit_detail["meg"])
 			} else {
 				list_path = list_path + sub_path[0]
@@ -71,15 +74,14 @@ func ListDeployments() gin.HandlerFunc { // for listing deployments and kit deta
 			fmt.Fprintf(os.Stderr, "ListDeployments path incorrect or tailing /: %v\n", sub_path)
 			context.JSON(http.StatusBadRequest, "Check your path.")
 		}
-
 		if len(sub_path) < 2 && sub_path[len(sub_path)-1] != "" { // getting list deployments or list kits
 			secret_list, err := client.Logical().List(list_path)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "client.Logical().List(%s): %+v\n", list_path, err)
-				context.JSON(http.StatusUnauthorized, err)
+				context.JSON(http.StatusUnauthorized, "Check your permission.")
 			} else if secret_list == nil {
 				fmt.Fprintf(os.Stderr, "client.Logical().List(%s): Secret List not found!\n", list_path)
-				context.JSON(http.StatusNotFound, "Marco: Give me my file! \n Polo: What file?")
+				context.JSON(http.StatusNotFound, "Marco: Give me "+sub_path[0]+"! Polo: What?")
 			} else {
 				deploys := secret_list.Data["keys"].([]interface{})
 				if sub_path[len(sub_path)-1] == "just list" {
@@ -87,9 +89,14 @@ func ListDeployments() gin.HandlerFunc { // for listing deployments and kit deta
 				} else {
 					detail_map := make(map[string]interface{})
 					for _, kit_name := range deploys {
-						path := sub_path[0] + "/" + kit_name.(string)
-						current_kit := LoadDeployments(path)
-						detail_map[kit_name.(string)] = current_kit["meg"]
+						path := sub_path[0] + "/"
+						name := kit_name.(string)
+						current_kit := LoadDeployments(path, name)
+						if current_kit["code"].(int) != http.StatusOK {
+							detail_map["error"] = current_kit["meg"]
+						} else {
+							detail_map[kit_name.(string)] = current_kit["meg"]
+						}
 					}
 					context.JSON(http.StatusOK, detail_map)
 				}
@@ -110,7 +117,6 @@ func CreatClient() *vault.Client {
 		os.Exit(1)
 	}
 	client.SetToken(os.Getenv("VAULT_TOKEN"))
-	// path := os.Getenv("VAULT_PREFIX")
 	if config.Error != nil {
 		fmt.Fprintf(os.Stderr, "CreatClient() config error: %+v\n", config.Error)
 		os.Exit(1)
