0660d07b3e0d598fb0658dc10b9f41239c8e1f76
Tanvie Kirane <67563504+tanviekirane@users.noreply.github.com>
2022-04-04 20:39:48 -0400

    select running into issues

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -2,11 +2,40 @@ import { Component } from "react";
 import { Card, Button, Row, Col, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import RouteMap from '../../RouteMap';
+import Select from 'react-select'
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deploymentList: [],
+            selectOptions: [],
+        }
+    }
+
+    componentDidMount = async () => {
+        this.fetchDeployments();
+    }
+
+    // componentDidMount() {
+    //     const url = "/list";
+    //     await fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         this.setState({
+    //             deploymentList: data["deploy_list"]
+    //         })
+    //     })
+    //     .catch(error => console.log(error))
+    // }
+
     render() {
         return (
             <div>
+                <Row>
+                    <h1>{this.state.deploymentList}</h1>
+                    <Select options={this.state.selectOptions}></Select>
+                </Row>
                 <Row className="m-4">
                     <Col xs={9} lg={9}>
                         <Form.Control type="text" placeholder="Search" />
@@ -18,6 +47,32 @@ class Dashboard extends Component {
             </div>
         )
     }
-}
 
+    fetchDeployments = async () => {
+        const url = "/list";
+        await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                deploymentList: data["deploy_list"]
+            })
+
+            const depList = data["deploy_list"]
+            const opt = []
+            for(var dep in depList){
+                opt.push(
+                    {
+                        value:dep,
+                        label:dep
+                    }
+                )
+            }
+            this.setState({
+                selectOptions: opt
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
+}
 export default Dashboard
