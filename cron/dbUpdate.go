50177bbe3aeaf89a87a2224474d973b7969c4a40
Sendil Balan <sendilbalan@gmail.com>
2022-04-18 11:45:31 -0400

    database connection for cron job

--- cron/dbUpdate.go
+++ cron/dbUpdate.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	vault "github.com/hashicorp/vault/api"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -85,4 +87,84 @@ func CreatClient() *vault.Client {
 		os.Exit(1)
 	}
 	return client
+}
+
+const (
+	host     = "postgres"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "genesis-ui"
+)
+
+func ConnectDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected!")
+	return db
+}
+
+func CheckAndUpdateDeployments(deployment_details map[string][]map[string]string) string {
+	// example structure for deployment_details
+	// var deploy_details = make(map[string][]map[string]string)
+	// kit_details := map[string]string{
+	// 	"name":        "bosh",
+	// 	"version":     "1.1.2",
+	// 	"deployed_at": "2021-11-17 03:31:14 +0000",
+	// }
+	// deploy_details["buffalo-lab"] = append(deploy_details["buffalo-lab"], kit_details)
+
+	dbConn := ConnectDB()
+	for deployment_name, element := range deployment_details {
+		for index, kit_details := range element {
+			fmt.Println("Processing index", index, "of deployment", deployment_name)
+			var deployment_id int
+			deployment_id = 0
+			err := dbConn.QueryRow(fmt.Sprintf("SELECT CASE WHEN COUNT(*) > 0 THEN (SELECT id FROM deployment_details WHERE name = '%s') ELSE 0 END FROM deployment_details WHERE name = '%s'", deployment_name, deployment_name)).Scan(&deployment_id)
+			if err != nil {
+				return err.Error()
+			}
+			if deployment_id == 0 {
+				err := dbConn.QueryRow(fmt.Sprintf("INSERT INTO deployment_details (name, recent_update_at) VALUES ('%s', now()) RETURNING id", deployment_name)).Scan(&deployment_id)
+				if err != nil {
+					return err.Error()
+				}
+			}
+			if deployment_id > 0 {
+				var kit_id int
+				kit_id = 0
+
+				err := dbConn.QueryRow(fmt.Sprintf("SELECT CASE WHEN COUNT(*) > 0 THEN (SELECT id FROM kit_details WHERE name = '%s' and deployment_id = %d) ELSE 0 END FROM kit_details WHERE name = '%s' and deployment_id = %d", kit_details["name"], deployment_id, kit_details["name"], deployment_id)).Scan(&kit_id)
+				if err != nil {
+					return err.Error()
+				}
+				fmt.Println("kit id", kit_id)
+				if kit_id == 0 {
+					err := dbConn.QueryRow(fmt.Sprintf("INSERT INTO kit_details (name, version, deployment_id, deployed_at, recent_update_at) VALUES ('%s', '%s', %d, '%s', now()) RETURNING id", kit_details["name"], kit_details["version"], deployment_id, kit_details["deployed_at"])).Scan(&kit_id)
+					if err != nil {
+						return err.Error()
+					}
+				} else if kit_id > 0 {
+					err := dbConn.QueryRow(fmt.Sprintf("UPDATE kit_details SET name='%s', version='%s', deployment_id=%d, deployed_at='%s', recent_update_at=now() WHERE deployment_id=%d and name = '%s'", kit_details["name"], kit_details["version"], deployment_id, kit_details["deployed_at"], deployment_id, kit_details["name"]))
+					if err != nil {
+						return "invalid update"
+					}
+				}
+			} else {
+				return "Deployment not inserted"
+			}
+		}
+	}
+	dbConn.Close()
+	return "Records imported successfullt!"
 }
