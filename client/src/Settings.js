b54b451156927904482677fd5fba30bbf1c363c2
Deepak Raghul Ravi <dravi@qarik.com>
2022-08-08 13:58:22 -0400

    updated the setting.js file to read the client id from environment variable

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -1,6 +1,6 @@
 let Settings = {
     currentPort: "http://localhost:3000",
-    githubClientId: "cfa78f27867826309fb6",
+    githubClientId: process.env.REACT_APP_CLIENT_ID,
 }
 
 export default Settings
